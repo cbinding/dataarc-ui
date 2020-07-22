5979352d121f278f23003e1b1418d05ea6a71f0c
Clayton Sexton <csbeasle@uark.edu>
2020-07-22 16:01:15 -0500

    Auth working with mongodb, all pages secured if not logged in
    #DARC-58; #DARC-64

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,24 +1,51 @@
 <template>
-  <div>
-    <b-navbar toggleable="lg" type="dark" variant="dark">
-      <b-container>
-        <b-navbar-brand href="/">DataARC Admin</b-navbar-brand>
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav class="ml-auto">
-            <b-navbar-nav>
-              <b-nav-item to="/">Home</b-nav-item>
-              <b-nav-item to="/about">About</b-nav-item>
-              <b-nav-item to="/pug">Pug</b-nav-item>
-              <b-nav-item to="/categories">Categories</b-nav-item>
-              <b-nav-item to="/login">Login</b-nav-item>
-            </b-navbar-nav>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-container>
-    </b-navbar>
-  </div>
+	<div>
+		<b-navbar toggleable="lg" type="dark" variant="dark">
+			<b-container>
+				<b-navbar-brand href="/">DataARC Admin</b-navbar-brand>
+				<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+				<b-collapse id="nav-collapse" is-nav>
+					<b-navbar-nav class="ml-auto">
+						<b-navbar-nav>
+							<b-nav-item to="/">Home</b-nav-item>
+							<b-nav-item to="/about">About</b-nav-item>
+							<b-nav-item to="/pug">Pug</b-nav-item>
+							<b-nav-item to="/categories">Categories</b-nav-item>
+							<b-nav-item v-if="user" @click="logout">Logout</b-nav-item>
+							<b-nav-item v-if="!user" to="/login">Login</b-nav-item>
+						</b-navbar-nav>
+					</b-navbar-nav>
+				</b-collapse>
+			</b-container>
+		</b-navbar>
+	</div>
 </template>
 <script>
-export default {};
+import VueJwtDecode from "vue-jwt-decode";
+export default {
+	data() {
+		return {
+			user: {},
+		};
+	},
+	methods: {
+		getUserDetails() {
+			let token = localStorage.getItem("jwt");
+			if (token) {
+				let decoded = VueJwtDecode.decode(token);
+				this.user = decoded;
+      }
+      else{
+        this.user = null;
+      }
+		},
+		logout() {
+			localStorage.removeItem("jwt");
+			this.$router.push("/login");
+		},
+	},
+	created() {
+		this.getUserDetails();
+	},
+};
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,68 @@
 <template>
-  <div class="login">
-    <form>
-      <h3>Sign In</h3>
+	<div>
+		<form class="login" @submit.prevent="login()">
+			<h3>Sign In</h3>
 
-      <div class="form-group">
-        <label>Email address</label>
-        <input type="email" class="form-control form-control-lg" />
-      </div>
+			<div class="form-group">
+				<label>Email address</label>
+				<input required v-model="email" type="email" class="form-control form-control-lg" />
+			</div>
 
-      <div class="form-group">
-        <label>Password</label>
-        <input type="password" class="form-control form-control-lg" />
-      </div>
+			<div class="form-group">
+				<label>Password</label>
+				<input required v-model="password" type="password" class="form-control form-control-lg" />
+			</div>
 
-      <button type="submit" class="btn btn-dark btn-lg btn-block">
-        Sign In
-      </button>
-    </form>
-  </div>
+			<button type="submit" class="btn btn-dark btn-lg btn-block">
+				Sign In
+			</button>
+
+			<p v-show="status">
+        <li class="help">
+          {{status}}
+        </li>
+      </p>
+		</form>
+	</div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {};
-  }
+	data() {
+		return {
+			email: "",
+			password: "",
+			status: false,
+		};
+	},
+	methods: {
+		login: function() {
+			const { email, password } = this;
+			// Request API.
+			axios
+				.post("http://localhost:1337/auth/local", {
+					identifier: email,
+					password: password,
+				})
+				.then((response) => {
+					// Handle success.
+					let token = response.data.jwt;
+					localStorage.setItem("jwt", token);
+					if (token) {
+						this.$router.push("/");
+					}
+				})
+				.catch((error) => {
+					// Handle error.
+					this.status = "Email or Password is invalid.";
+				});
+		},
+	},
 };
 </script>
+
+<style lang="scss">
+.help{
+  color: #d9534f;
+}
+</style>
