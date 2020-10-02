062740a7ec3e3b76af4badd73f4835f213533149
Christopher Angel <cangel@uark.edu>
2020-10-02 14:44:28 -0500

    Some basic refactor - no behavior changes

--- src/views/Collections/Concepts.vue
+++ src/views/Collections/Concepts.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container fluid>
-    <router-view/>
+    <router-view />
     <table-view-layout
       v-if="$route.name === 'Concepts'"
       :rows="rows"
@@ -25,7 +25,6 @@
         <b-table
           v-if="concepts"
           ref="concepts"
-          id="concepts"
           :busy="conceptsLoading"
           :per-page="perPage"
           :current-page="currentPage"
@@ -39,44 +38,74 @@
         >
           <template v-slot:table-busy>
             <div class="text-center my-2">
-              <b-spinner class="align-middle"></b-spinner>
+              <b-spinner class="align-middle" />
               <strong>Loading...</strong>
             </div>
           </template>
           <template v-slot:cell(title)="row">
-            <div class="text-wrap" style="width: 200px; max-width: 200px;" v-if="row.item.title">
+            <div
+              v-if="row.item.title"
+              class="text-wrap"
+              style="width: 200px; max-width: 200px;"
+            >
               {{ row.item.title }}
             </div>
           </template>
           <template v-slot:cell(description)="row">
-            <div class="text-wrap" style="width: 400px; max-width: 450px;" v-if="row.item.description">
+            <div
+              v-if="row.item.description"
+              class="text-wrap"
+              style="width: 400px; max-width: 450px;"
+            >
               {{ shorten(row.item.description) }}
             </div>
           </template>
           <template v-slot:head(combinators_count)="row">
-            <div class="text-center"># Combinators</div>
+            <div class="text-center">
+              # Combinators
+            </div>
           </template>
           <template v-slot:cell(combinators_count)="row">
             <div class="text-center">
-              <b-badge pill variant="primary">
+              <b-badge
+                pill
+                variant="primary"
+              >
                 {{ row.item.combinators_count }}
               </b-badge>
             </div>
           </template>
           <template v-slot:head(topics_count)="row">
-            <div class="text-center"># Topics</div>
+            <div class="text-center">
+              # Topics
+            </div>
           </template>
           <template v-slot:cell(topics_count)="row">
             <div class="text-center">
-              <b-badge pill variant="primary">
+              <b-badge
+                pill
+                variant="primary"
+              >
                 {{ row.item.topics_count ? row.item.topics_count : 0 }}
               </b-badge>
             </div>
           </template>
-          <template v-slot:cell(actions)="row" class="actions">
+          <template
+            v-slot:cell(actions)="row"
+            class="actions"
+          >
             <b-button-group size="sm">
-              <b-button :to="{name: 'Update Concept', params: {id: row.item.id} }" variant="primary" v-text="'Edit'"></b-button>
-              <b-button variant="primary" v-text="'Delete'" @click="itemToDelete = row.item" v-b-modal.deleteConfirmation></b-button>
+              <b-button
+                :to="{name: 'Update Concept', params: {id: row.item.id} }"
+                variant="primary"
+                v-text="'Edit'"
+              />
+              <b-button
+                v-b-modal.deleteConfirmation
+                variant="primary"
+                @click="itemToDelete = row.item"
+                v-text="'Delete'"
+              />
             </b-button-group>
           </template>
         </b-table>
