33fe72422888b8e07b94f0018e58a00b40770609
Tim Sexton <tlsexto@gmail.com>
2020-11-05 16:54:04 -0600

    Remove category name field

--- src/views/Collections/Categories.vue
+++ src/views/Collections/Categories.vue
@@ -14,10 +14,7 @@
       @limitUpdated="updateLimit"
     >
       <template v-slot:button>
-        <b-button
-          variant="primary"
-          :to="{name: 'Create Category' }"
-        >
+        <b-button variant="primary" :to="{ name: 'Create Category' }">
           <b-icon-plus />Add new Category
         </b-button>
       </template>
@@ -57,8 +54,17 @@
           </template>
           <template v-slot:cell(actions)="row" class="actions">
             <b-button-group size="sm">
-              <b-button variant="primary" v-text="'Edit'" :to="{name: 'Update Category', params: {id: row.item.id} }"></b-button>
-              <b-button variant="primary" v-text="'Delete'" @click="itemToDelete = row.item" v-b-modal.deleteConfirmation></b-button>
+              <b-button
+                variant="primary"
+                v-text="'Edit'"
+                :to="{ name: 'Update Category', params: { id: row.item.id } }"
+              ></b-button>
+              <b-button
+                variant="primary"
+                v-text="'Delete'"
+                @click="itemToDelete = row.item"
+                v-b-modal.deleteConfirmation
+              ></b-button>
             </b-button-group>
           </template>
         </b-table>
@@ -68,43 +74,40 @@
 </template>
 
 <script>
-import collectionMixin from '../../mixins/collectionMixin'
+import collectionMixin from '../../mixins/collectionMixin';
 export default {
-  components: {
-  },
+  components: {},
   mixins: [collectionMixin],
   data() {
     return {
       component: 'Categories',
       displayFields: [
         { key: 'actions', sortable: false },
-        { key: 'name', sortable: true },
         { key: 'title', sortable: true },
         { key: 'description', sortable: true },
         { key: 'color', sortable: true },
-        { key: 'datasets_count', sortable: true },
+        { key: 'datasets_count', sortable: true }
       ],
-      categoriesLoading: true,
-    }
+      categoriesLoading: true
+    };
   },
   watch: {
     categories(val) {
       if (val) {
-        this.categoriesLoading = this.loadingState(val.length)
+        this.categoriesLoading = this.loadingState(val.length);
       }
     },
     $route(to, from) {
       if (to.name !== 'Categories') {
-        this.$apollo.queries.allCategories.skip = true
+        this.$apollo.queries.allCategories.skip = true;
+      } else if (from.name !== 'Categories') {
+        this.$apollo.queries.allCategories.skip = false;
+        this.$apollo.queries.allCategories.refetch();
       }
-      else if (from.name !== 'Categories') {
-        this.$apollo.queries.allCategories.skip = false
-        this.$apollo.queries.allCategories.refetch()
-      }
-    },
+    }
   },
   created() {
-    this.$apollo.queries.allCategories.skip = false
-  },
-}
+    this.$apollo.queries.allCategories.skip = false;
+  }
+};
 </script>
