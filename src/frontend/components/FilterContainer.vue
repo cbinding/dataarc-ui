25d7c27a15e4f15111a84c123126a71b1438ecfe
Tim Sexton <tlsexto@gmail.com>
2020-11-15 23:54:42 -0600

    Quick fix for timeline range in filter list

--- src/frontend/components/FilterContainer.vue
+++ src/frontend/components/FilterContainer.vue
@@ -5,24 +5,42 @@
         <b-col>
           <h2>
             Filters
-            <sup><a href="http://www.data-arc.org/filters/" title="How dataarc thinks about filters" class="text-dark" target="_blank" data-toggle="tooltip" ><b-icon-info-circle-fill /></a></sup>
+            <sup
+              ><a
+                href="http://www.data-arc.org/filters/"
+                title="How dataarc thinks about filters"
+                class="text-dark"
+                target="_blank"
+                data-toggle="tooltip"
+                ><b-icon-info-circle-fill /></a
+            ></sup>
           </h2>
-          <hr class="primary">
-          <p>Filters will appear here as you select them from the sections above.</p>
+          <hr class="primary" />
+          <p>
+            Filters will appear here as you select them from the sections above.
+          </p>
         </b-col>
       </b-row>
       <b-row>
         <b-col class="mt-3 mb-3">
           <b-card-group deck id="filters" v-if="filters">
-
             <!-- Spatial -->
-            <b-card header="Spatial" v-if="filters.box || filters.polygon" bg-variant="dark" text-variant="white" no-body>
+            <b-card
+              header="Spatial"
+              v-if="filters.box || filters.polygon"
+              bg-variant="dark"
+              text-variant="white"
+              no-body
+            >
               <b-list-group data-type="spatial" v-if="filters.box" flush>
                 <b-list-group-item
                   class="d-flex justify-content-between align-items-center text-left bg-transparent"
                 >
                   <small>Bounding Box {{ filters.box }}</small>
-                  <b-icon-x-circle-fill variant="light" @click="$emit('removed', 'box')" />
+                  <b-icon-x-circle-fill
+                    variant="light"
+                    @click="$emit('removed', 'box')"
+                  />
                 </b-list-group-item>
               </b-list-group>
               <b-list-group data-type="spatial" v-if="filters.polygon" flush>
@@ -30,29 +48,47 @@
                   class="d-flex justify-content-between align-items-center text-left bg-transparent"
                 >
                   <small>Polygon {{ shorten(filters.polygon) }} ...</small>
-                  <b-icon-x-circle-fill variant="light" @click="$emit('removed', 'polygon')" />
+                  <b-icon-x-circle-fill
+                    variant="light"
+                    @click="$emit('removed', 'polygon')"
+                  />
                 </b-list-group-item>
               </b-list-group>
             </b-card>
             <!-- /Spatial -->
 
             <!-- Temporal -->
-            <b-card header="Temporal" v-if="filters.temporal && filters.temporal.length" bg-variant="dark" text-variant="white" no-body>
+            <b-card
+              header="Temporal"
+              v-if="filters.temporal && filters.temporal.length"
+              bg-variant="dark"
+              text-variant="white"
+              no-body
+            >
               <b-list-group data-type="temporal" flush>
                 <b-list-group-item
                   :key="index"
-                  v-for="(filter,  index) in filters.temporal"
+                  v-for="(filter, index) in filters.temporal"
                   class="d-flex justify-content-between align-items-center text-left bg-transparent"
                 >
-                  Range: {{ filter.start }} - {{ filter.end }}
-                  <b-icon-x-circle-fill variant="light" @click="$emit('removed', 'temporal', index)" />
+                  Range: {{ filter.begin }} - {{ filter.end }}
+                  <b-icon-x-circle-fill
+                    variant="light"
+                    @click="$emit('removed', 'temporal', index)"
+                  />
                 </b-list-group-item>
               </b-list-group>
             </b-card>
             <!-- /Temporal -->
 
             <!-- Concepts -->
-            <b-card header="Concepts" v-if="filters.concept && conceptFilters && conceptFilters.length" bg-variant="dark" text-variant="white" no-body>
+            <b-card
+              header="Concepts"
+              v-if="filters.concept && conceptFilters && conceptFilters.length"
+              bg-variant="dark"
+              text-variant="white"
+              no-body
+            >
               <b-list-group data-type="concepts" flush>
                 <b-list-group-item
                   v-for="(filter, index) in conceptFilters"
@@ -60,14 +96,23 @@
                   class="d-flex justify-content-between align-items-center text-left bg-transparent"
                 >
                   {{ filter.label }}
-                  <b-icon-x-circle-fill variant="light" @click="$emit('removed', 'concept', index)" />
+                  <b-icon-x-circle-fill
+                    variant="light"
+                    @click="$emit('removed', 'concept', index)"
+                  />
                 </b-list-group-item>
               </b-list-group>
             </b-card>
             <!-- /Concepts -->
 
             <!-- Keywords -->
-            <b-card header="Keywords" v-if="filters.keyword" bg-variant="dark" text-variant="white" no-body>
+            <b-card
+              header="Keywords"
+              v-if="filters.keyword"
+              bg-variant="dark"
+              text-variant="white"
+              no-body
+            >
               <b-list-group data-type="keyword" flush>
                 <b-list-group-item
                   :key="index"
@@ -75,14 +120,15 @@
                   class="d-flex justify-content-between align-items-center text-left bg-transparent"
                 >
                   {{ filter }}
-                  <b-icon-x-circle-fill variant="light" @click="$emit('removed', 'keyword', index)" />
+                  <b-icon-x-circle-fill
+                    variant="light"
+                    @click="$emit('removed', 'keyword', index)"
+                  />
                 </b-list-group-item>
               </b-list-group>
             </b-card>
             <!-- /Keywords -->
-
           </b-card-group>
-
         </b-col>
       </b-row>
       <b-row>
@@ -95,12 +141,18 @@
             >
               <b-icon-upload aria-hidden="true" /> Upload Filters
             </b-button>
-            <input type="file"
+            <input
+              type="file"
               @change="readFile"
               style="display:none"
               ref="fileUpload"
-            >
-            <a ref="downloadAnchor" :href="filterDownloadData" :download="downloadName" style="display:none"></a>
+            />
+            <a
+              ref="downloadAnchor"
+              :href="filterDownloadData"
+              :download="downloadName"
+              style="display:none"
+            ></a>
             <b-button
               title="Download Filters"
               variant="success"
@@ -108,10 +160,7 @@
             >
               <b-icon-download aria-hidden="true" /> Download Filters
             </b-button>
-            <b-button
-              title="Share Search"
-              variant="primary"
-            >
+            <b-button title="Share Search" variant="primary">
               <b-icon-share aria-hidden="true" /> Share Search
             </b-button>
           </b-button-group>
@@ -126,61 +175,62 @@ export default {
   props: {
     filters: {
       type: Object,
-      required: true,
+      required: true
     },
     conceptFilters: {
       type: Array,
-      required: true,
-    },
+      required: true
+    }
   },
   data() {
     return {
       downloadName: 'filters.json'
-    }
+    };
   },
   computed: {
     filterDownloadData() {
-      let stringFilters = ''
+      let stringFilters = '';
       if (!this.filters || !Object.keys(this.filters).length > 0) {
-        stringFilters = JSON.stringify({})
+        stringFilters = JSON.stringify({});
       } else {
-        stringFilters = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(this.filters))}`
+        stringFilters = `data:text/json;charset=utf-8,${encodeURIComponent(
+          JSON.stringify(this.filters)
+        )}`;
       }
-      return stringFilters
+      return stringFilters;
     }
   },
   methods: {
     readFile(e) {
-      const file = e.target.files[0]
+      const file = e.target.files[0];
 
-      const reader = new FileReader()
+      const reader = new FileReader();
 
-      reader.readAsText(file)
-      let _this = this
-      reader.onload = function () {
-        _this.loadPreviousSearch(reader.result)
-      }
+      reader.readAsText(file);
+      let _this = this;
+      reader.onload = function() {
+        _this.loadPreviousSearch(reader.result);
+      };
 
-      reader.onerror = function () {
-        console.log(reader.error)
-      }
+      reader.onerror = function() {
+        console.log(reader.error);
+      };
     },
     loadPreviousSearch(data) {
-      this.$emit('filters-loaded', JSON.parse(data))
+      this.$emit('filters-loaded', JSON.parse(data));
     },
     startFileUpload() {
-      this.$refs.fileUpload.click()
+      this.$refs.fileUpload.click();
     },
     shorten(val) {
-      return val.slice(0, 3)
+      return val.slice(0, 3);
     },
     saveSearchFilters() {
-      console.log("Save search filters")
-      this.$refs.downloadAnchor.click()
-    },
+      console.log('Save search filters');
+      this.$refs.downloadAnchor.click();
+    }
   }
 };
 </script>
 
-<style>
-</style>
+<style></style>
