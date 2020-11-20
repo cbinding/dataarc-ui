8e9d2c0be4b91d22d905dd6e1265cd792168aa61
Clayton Sexton <csbeasle@uark.edu>
2020-11-20 13:50:58 -0600

    Move timeline form below viz, range updates begin/end date in form
    Sets "Time Period" dropdown selection to "Selected Range"

--- src/frontend/components/TimelineContainer.vue
+++ src/frontend/components/TimelineContainer.vue
@@ -30,106 +30,70 @@
           <timeline :triggers="triggers" :filters="filters" :width="timelineWidth" v-model="currentSelectedRange" />
         </b-col>
       </b-row>
-      <b-row>
-        <b-col align-self="center">
-          <b-button-group>
-            <b-button
-              title="View Temporal Filter Options"
-              variant="primary"
-              data-toggle="modal"
-              v-b-modal.timeline-filter
-            >
-              <b-icon-filter aria-hidden="true" /> View Temporal Filter Options
-            </b-button>
-            <b-button
-              title="Add Selected Range To Filter"
-              variant="success"
-              v-show="currentSelectedRange"
-              :disabled="checkDuplicateRange()"
-              @click.prevent="setSelectedRange"
-            >
-              <b-icon-plus aria-hidden="true" /> Add Selected Range To Filter
-            </b-button>
-          </b-button-group>
-        </b-col>
-      </b-row>
     </b-container>
-
-    <b-modal
-      id="timeline-filter"
-      class="modal-dialog"
-      aria-hidden="true"
-      size="md"
-      header-bg-variant="dark"
-      header-text-variant="light"
-      hide-header-close
-    >
-      <template class="modal-title" #modal-title>
-        Temporal Filter
-      </template>
-      <template #default>
-        <p>Select a pre-defined time period from the list or set a custom begin and end date below.</p>
-        <form>
-          <div class="form-row">
-            <div class="col mb-3">
-              <label for="timeline-period">Time Period</label>
-              <select
-                id="timeline-period"
-                class="custom-select mr-sm-2"
-                @change="setTimelineFilterPeriod"
-                v-model="selectedFilterIndex"
-              >
-                <option v-for="(coverage, index) in temporalCoverages" :key="index" :value="index">
-                  {{ coverage.title }}
-                </option>
-              </select>
+    <b-row class="justify-content-center">
+      <b-col sm="4" class="shadow">
+        <b-card>
+          <p>Select a pre-defined time period from the list or set a custom begin and end date below.</p>
+          <form>
+            <div class="form-row">
+              <div class="col mb-3">
+                <label for="timeline-period">Time Period</label>
+                <select
+                  id="timeline-period"
+                  class="custom-select mr-sm-2"
+                  @change="setTimelineFilterPeriod"
+                  v-model="selectedFilterIndex"
+                >
+                  <option v-for="(coverage, index) in temporalCoverages" :key="index" :value="index">
+                    {{ coverage.title }}
+                  </option>
+                  <option value="Selected Range">
+                    Selected Range
+                  </option>
+                </select>
+              </div>
             </div>
-          </div>
-          <div class="form-row">
-            <div class="col mb-3">
-              <label for="timeline-startdate">Begin Date *</label>
-              <input
-                id="timeline-begindate"
-                type="text"
-                class="form-control"
-                placeholder="begin"
-                v-model="selectedFilter.begin"
-              >
+            <div class="form-row">
+              <div class="col mb-3">
+                <label for="timeline-startdate">Begin Date *</label>
+                <input
+                  id="timeline-begindate"
+                  type="text"
+                  class="form-control"
+                  placeholder="begin"
+                  v-model="selectedFilter.begin"
+                >
+              </div>
+              <div class="col mb-3">
+                <label for="timeline-enddate">End Date *</label>
+                <input
+                  id="timeline-enddate"
+                  type="text"
+                  class="form-control"
+                  placeholder="end"
+                  v-model="selectedFilter.end"
+                >
+              </div>
             </div>
-            <div class="col mb-3">
-              <label for="timeline-enddate">End Date *</label>
-              <input
-                id="timeline-enddate"
-                type="text"
-                class="form-control"
-                placeholder="end"
-                v-model="selectedFilter.end"
-              >
-            </div>
-          </div>
-        </form>
-        <p class="text-muted small">
-          * A negative number is used to represent B.C.E. dates. E.g. -1000 = 1000 B.C.E.
-        </p>
-      </template>
-      <template #modal-footer>
-        <button
-          type="button"
-          class="btn btn-secondary"
-          @click="$bvModal.hide('timeline-filter')"
-        >
-          Cancel
-        </button>
-        <button
-          type="button"
-          class="btn btn-success"
-          :disabled="checkDuplicateTemporal()"
-          @click="applyFilter"
-        >
-          Apply Filter
-        </button>
-      </template>
-    </b-modal>
+          </form>
+          <p class="text-muted small">
+            * A negative number is used to represent B.C.E. dates. E.g. -1000 = 1000 B.C.E.
+          </p>
+          <b-row align-h="center">
+            <b-button
+              type="button"
+              class="btn btn-success"
+              :disabled="checkDuplicateTemporal()"
+              @click="applyFilter"
+            >
+              + Add Temporal Filter
+            </b-button>
+          </b-row>
+        </b-card>
+      </b-col>
+    </b-row>
+    <br>
   </section>
 </template>
 
@@ -164,6 +128,12 @@ export default {
       currentSelectedRange: null
     }
   },
+  watch: {
+    currentSelectedRange(val) {
+      this.selectedFilterIndex = 'Selected Range'
+      this.selectedFilter = val
+    }
+  },
   mounted() {
     this.timelineWidth = this.$refs.timelineContainer.clientWidth
     this.getTemporalCoverages()
@@ -184,36 +154,13 @@ export default {
       }
       return true
     },
-    checkDuplicateRange() {
-      let exists = []
-      if (!this.currentSelectedRange || this.currentSelectedRange.end === null || this.currentSelectedRange.begin === null) {
-        return true
-      }
-      if (this.filters && this.filters.temporal) {
-        exists = this.filters.temporal.filter((filter) => {
-          return (filter.end === this.currentSelectedRange.end) && (filter.begin === this.currentSelectedRange.begin)
-        })
-      }
-      if (exists.length === 0) {
-        return false
-      }
-      return true
-    },
     setTimelineFilterPeriod() {
       const time = this.temporalCoverages[this.selectedFilterIndex]
       this.selectedFilter.begin = time.begin
       this.selectedFilter.end = time.end
     },
     applyFilter(evt) {
-      evt.preventDefault()
       this.timelineFilter.push({ ...this.selectedFilter })
-
-      this.emitFilter()
-      // console.log(val);
-      this.$bvModal.hide('timeline-filter')
-    },
-    setSelectedRange() {
-      this.timelineFilter.push({ ...this.currentSelectedRange })
       this.emitFilter()
     },
     emitFilter() {
